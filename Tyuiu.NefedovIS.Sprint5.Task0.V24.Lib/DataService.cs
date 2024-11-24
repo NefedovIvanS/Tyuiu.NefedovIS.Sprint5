a597a46d9efe0f69ee08072c73e0636554de0dc9
Иван Нефедов <Tyuiu.NefedovIS@outlook.com>
2024-11-24 12:15:03 +0500

    Task0

--- Tyuiu.NefedovIS.Sprint5.Task0.V24.Lib/DataService.cs
+++ Tyuiu.NefedovIS.Sprint5.Task0.V24.Lib/DataService.cs
@@ -6,8 +6,8 @@ namespace Tyuiu.NefedovIS.Sprint5.Task0.V24.Lib
     {
         public string SaveToFileTextData(int x)
         {
-            //string path = Path.Combine(Path.GetTempPath(), "OutputFileTask0.txt");
-            string path = $@"{Directory.GetCurrentDirectory()}\OutputFileTask0.txt";
+            string path = Path.Combine(Path.GetTempPath(), "OutputFileTask0.txt");
+            //string path = $@"{Directory.GetCurrentDirectory()}\OutputFileTask0.txt";
             double z = (Math.Pow(x, 3) - 8) / (2 * Math.Pow(x, 2));
             File.WriteAllText(path, Convert.ToString(Math.Round(z, 3)));
             return path;
