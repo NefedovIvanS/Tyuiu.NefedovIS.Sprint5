222b4c490d09906270c30a0829f31b2b62f1c074
NefedovIvanS <Tyuiu.NefedovIS@outlook.com>
2024-11-26 00:12:24 +0500

    Task4

---
+++ Tyuiu.NefedovIS.Sprint5.Task4.V20.Lib/DataService.cs
@@ -0,0 +1,15 @@
+﻿using System.Globalization;
+using tyuiu.cources.programming.interfaces.Sprint5;
+namespace Tyuiu.NefedovIS.Sprint5.Task4.V20.Lib
+{
+    public class DataService : ISprint5Task4V20
+    {
+        public double LoadFromDataFile(string path)
+        {
+            string strX = File.ReadAllText(path);
+            double x = double.Parse(strX, CultureInfo.InvariantCulture);
+            double res = Math.Round(Math.Pow(((Math.Pow(x,2))/(Math.Sin(x))), 3), 2);
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.NefedovIS.Sprint5.Task4.V20.Test/DataServiceTest.cs
@@ -0,0 +1,11 @@
+namespace Tyuiu.NefedovIS.Sprint5.Task4.V20.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
