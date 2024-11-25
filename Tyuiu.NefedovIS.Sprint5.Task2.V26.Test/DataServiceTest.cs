fedcfff7bc5afbe7fba37bd5e60b3d67bd82356e
NefedovIvanS <Tyuiu.NefedovIS@outlook.com>
2024-11-25 12:41:37 +0500

    Task2

---
+++ Tyuiu.NefedovIS.Sprint5.Task2.V26.Test/DataServiceTest.cs
@@ -0,0 +1,11 @@
+namespace Tyuiu.NefedovIS.Sprint5.Task2.V26.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Tyuiu.NefedovIS.Sprint5.Task2.V26/Program.cs
@@ -0,0 +1,31 @@
+﻿using Tyuiu.NefedovIS.Sprint5.Task2.V26.Lib;
+namespace Tyuiu.NefedovIS.Sprint5.Task2.V26
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            int[,] mtrx = new int[3, 3] { { 2, -3 , -3 },
+            { -8, 8 , 0 }, { -6, 1 , 0 } };
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************\n");
+
+            Console.WriteLine($"matrix = {mtrx}");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            string res = ds.SaveToFileTextData(mtrx);
+
+
+            Console.WriteLine("Файл " + res);
+            Console.WriteLine("Создан!");
+            Console.ReadKey();
+        }
+    }
+}
