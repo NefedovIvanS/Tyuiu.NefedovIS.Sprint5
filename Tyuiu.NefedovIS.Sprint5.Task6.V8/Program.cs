dc738afadccbcd6065f37b46164267ea3d88863c
NefedovIvanS <Tyuiu.NefedovIS@outlook.com>
2024-11-26 19:17:26 +0500

    Task6

---
+++ Tyuiu.NefedovIS.Sprint5.Task6.V8/Program.cs
@@ -0,0 +1,26 @@
+﻿using Tyuiu.NefedovIS.Sprint5.Task6.V8.Lib;
+namespace Tyuiu.NefedovIS.Sprint5.Task6.V8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "C:/DataSprint5/InPutDataFileTask6V8.txt";
+            DataService ds = new DataService();
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************\n");
+
+            Console.WriteLine($"Путь = {path}");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            double res = ds.LoadFromDataFile(path);
+            Console.WriteLine(res);
+            Console.ReadKey();
+        }
+    }
+}
